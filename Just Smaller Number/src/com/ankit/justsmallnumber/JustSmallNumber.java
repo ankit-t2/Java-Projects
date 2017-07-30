eb30f379b8c0c4b9c0f66ae4f44a27e7bbbfbd8c
ankit-t2 <ankit.t2@gmail.com>
2017-07-30 18:50:44 +0530

    Remove unnecessary imports

--- Just Smaller Number/src/com/ankit/justsmallnumber/JustSmallNumber.java
+++ Just Smaller Number/src/com/ankit/justsmallnumber/JustSmallNumber.java
@@ -2,8 +2,6 @@ package com.ankit.justsmallnumber;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.TreeSet;
 
 public class JustSmallNumber {
